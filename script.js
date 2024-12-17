d8580ab9735935a702360dff96518b4301dfff5f
Tayyaba Rasheed <kaneezamna234@gmail.com>
2024-12-17 21:02:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ btn.onclick = () =>{
     if(document.body.style.backgroundColor === "white"){
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white";
+        btn.style.borderRadius = "25px";
+        btn.style.backgroundColor = "grey"
         btn.textContent = "Switch to light mode";
     }
     else{
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
+        btn.style.borderRadius = "8px";
+        btn.style.backgroundColor = "cadetblue";
         btn.textContent = "Switch to dark mode";
     }
 }
